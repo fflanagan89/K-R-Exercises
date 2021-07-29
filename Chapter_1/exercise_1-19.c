4894e971b7e8a90fc15b0ac65f0c475979471bf2
fflanagan89 <francis.flanagan@uvm.edu>
2021-07-29 13:58:18 -0400

    Completed exercise 1-19. Implemented function reverse. Had to make change to function name "getline" to "get_line" due to that function existing in stdio.h (i think)

---
+++ Chapter_1/exercise_1-19.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#define MAXLINE 1000
+
+int get_line(char line[], int maxline);
+void reverse(char line[], int length);
+
+
+main(){
+
+    int len;
+    int max;
+    char line[MAXLINE];
+
+    while((len = get_line(line, MAXLINE)) > 0){
+        reverse(line, len);
+        printf("\n %s\n", line);
+
+    }
+
+    
+
+}
+
+int get_line(char s[], int lim){
+
+    int c, i;
+    for(i = 0; i < lim-1 && (c=getchar())!=EOF && c!= '\n'; ++i){
+        s[i] = c;
+    }
+    if(c == '\n'){
+        s[i] = c;
+        ++i;           
+    }
+    s[i] = '\0';
+    return i;
+
+}
+
+void reverse(char s[], int len){
+
+    for(int i = 0; i <= len; ++i, --len) {
+
+        char c = s[i];
+        s[i] = s[len-1];
+        s[len-1] = c;
+    
+    }
+
+}
+
